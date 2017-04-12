1bf82891d66555cbeb80570ae595996cab178bf0
Wasil Zafar <wasil.zafar@gmail.com>
2017-04-12 20:22:01 +0530

    Working RequireJS

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-requirejs.config({
-  baseUrl: 'libs',
-  shim: {
-    'jquery-3.2.1.min': {
-      deps: ['underscore-min', 'jquery-3.2.1.min'],
-      exports: 'Backbone'
-    },
-    'underscore-min': {
-      exports: '_'
-    },
-    'jquery-3.2.1.min': {
-      exports: '$'
-    }
-  }
-});
-
-requirejs(['jquery-3.2.1.min'], function($) {
-  $('body').html('Hello World .... Again!');
+require(['jquery'], function($) {
+  $('body').html('Hello World, again !');
 });
